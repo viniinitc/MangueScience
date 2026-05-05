7870908f7f11e5dd109ec5c114cb4f7634f257ba
viniinitc <vinitcavalcantena@gmail.com>
2026-05-05 03:05:30 -0300

    Comeco do desenvelvimento dos projeteis

--- src/main.c
+++ src/main.c
@@ -8,7 +8,10 @@
 typedef struct balls{
 
 	int type;
-	int* dir;
+	//qual direcao ele ta andando, por exemplo se ele tiver indo de baixo pra cima a array ficaria {1,0,0,0} ja que UP eh o primeiro da ordem
+	//quando vcs acharam que precisar mudar algo na lista nao esquecam de mudar na funcao de inicializacao
+	//ORDEM DE DIRECOES UP DOWN RIGHT LEFT, TENTEM NAO MUDAR ISSO
+	int dir[4];
 	float posx;
 	float posy;
 	Texture2D sprite;
@@ -22,33 +25,88 @@ typedef struct songs{
 	int qntbeats;
 
 }songs;
+void getdirectionofball(int* array){
 
-void createnextball(balls** head, int type, int* dir,Texture2D sprite, float posx, float posy){
+	int dir = GetRandomValue(1,4);
+	
+
+	if(dir = 1) {
+		array[0] = 1;
+		array[1] = 0;
+		array[2] = 0;
+		array[3] = 0;
+	}
+	if(dir = 2) {
+		array[0] = 0;
+		array[1] = 1;
+		array[2] = 0;
+		array[3] = 0;
+	}
+	if(dir = 3) {
+		array[0] = 0;
+		array[1] = 0;
+		array[2] = 1;
+		array[3] = 0;
+	}
+	if(dir = 4){
+		array[0] = 0;
+		array[1] = 0;
+		array[2] = 0;
+		array[3] = 1;
+	}
+
+
+
+}
+
+void createnextball(balls** head,balls** tail, int type,Texture2D sprite, float posx, float posy){
 
 	if(*head == NULL) {
 
 		*head = (balls*)malloc(sizeof(balls));
 		if (*head == NULL) return;
+		(*head)->next = NULL;
 		(*head)->type = type;
-		(*head)->dir = dir;
+
+		getdirectionofball((*head)->dir);
+		
 		(*head)->posx = posx;
 		(*head)->posy = posy;
 		(*head)->sprite = sprite;
+		*tail = *head;
 		return;
 	}
 
-	balls* n = *head;
-
-	while(n->next != NULL) n = n->next;
+	balls* n = *tail;
 	n->next = (balls*)malloc(sizeof(balls));
 	if(n->next == NULL) return;
 	n->next->type = type;
-	n->next->dir = dir;
+	
+	getdirectionofball(n->next->dir);
+	
 	n->next->posx = posx;
 	n->next->posy = posy;
 	n->next->sprite = sprite;
+	n->next->next = NULL;
+	*tail = n->next;
 
 }
+
+void deleteeverything(balls** head, balls** tail){
+
+	balls* n = *head;
+
+	while(n != NULL){
+
+		UnloadTexture(n->sprite);
+		balls* aux = n;
+		n = n->next;
+		free(aux);
+
+	}
+}
+
+
 
 
 
@@ -58,11 +116,12 @@ int main ()
 	
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
+	SetRandomSeed(10);
 	
 	
-
-	InitWindow(1260, 800, "Hello Raylib");
+	InitWindow(1280, 800, "Hello Raylib");
 	InitAudioDevice();
+
 
 	//tamanho da tela
 	int screenheight = GetScreenHeight();
@@ -70,13 +129,30 @@ int main ()
 
 	SearchAndSetResourceDir("resources");
 
-	Music musictest = LoadMusicStream("praiera.mp3");
+
+	//definicao de musicas
+	songs praiera;
+	praiera.musica = LoadMusicStream("praiera.mp3");
+	praiera.qntbeats = 452;
+	PlayMusicStream(praiera.musica);
+	SetMusicVolume(praiera.musica, 1.0);
+	// musica 1 acima
+
+	balls* head = NULL;
+	balls* tail = NULL;
+
 	
+
+	for(int i = 0; i < praiera.qntbeats; i++){
+		//random da funcao n ta funcionando
+		createnextball(&head,&tail, 0,LoadTexture("balltest.png"),0,0);
+
+	}
+
+	
+
+	//textura do jogador
 	Texture wabbit = LoadTexture("wabbit_alpha.png");
-	PlayMusicStream(musictest);
-
-	SetMusicVolume(musictest, 1.0);
-
 
 	//posicao do jogador
 	float pposx = screenwidth/2;
@@ -123,17 +199,24 @@ int main ()
 
 		BeginDrawing();
 
-		UpdateMusicStream(musictest);
+		UpdateMusicStream(praiera.musica);
 
 		
 		ClearBackground(BLACK);
 
 		DrawText("Hello Raylib", 200,200,20,WHITE);
 
-		if(IsMusicStreamPlaying(musictest))DrawText("aeu",300,300, 20, WHITE );
+		if(IsMusicStreamPlaying(praiera.musica))DrawText("aeu",300,300, 20, WHITE );
 
-		// draw our texture to the screen
+
 		DrawTexture(wabbit, pposx, pposy, WHITE);
+
+		DrawTexture(head->sprite, 100, 100, WHITE);
+
+		if(head->dir[0])DrawText("1000",400, 400, 20, WHITE);
+		if(head->dir[1])DrawText("0100",400, 400, 20, WHITE);
+		if(head->dir[2])DrawText("0010",400, 400, 20, WHITE);
+		if(head->dir[3])DrawText("0001",400, 400, 20, WHITE);
 
 		// posicao onde o jogador vai pegar as notas
 		if(up)DrawRectangle(pposx, pposy - 20, 10, 2, RED);
@@ -141,17 +224,16 @@ int main ()
 		if(right)DrawRectangle(pposx + 50, pposy, 2, 10, RED);
 		if(left)DrawRectangle(pposx - 20, pposy, 2, 10, RED);
 		
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
+	
 		EndDrawing();
 	}
 
-	// cleanup
-	// unload our texture so it can be cleaned up
+
 	UnloadTexture(wabbit);
+	deleteeverything(&head, &tail);
+	StopMusicStream(praiera.musica);
 
-	StopMusicStream(musictest);
-
-	UnloadMusicStream(musictest);
+	UnloadMusicStream(praiera.musica);
 
 	CloseAudioDevice();
 	CloseWindow();
