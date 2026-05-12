d21b93076ce748f6a0ca41fd64ce0485adf705e7
viniinitc <vinitcavalcantena@gmail.com>
2026-05-12 10:04:51 -0300

    movimentacao e atualizacao das notas

--- src/main.c
+++ src/main.c
@@ -12,9 +12,9 @@ typedef struct balls{
 	//quando vcs acharam que precisar mudar algo na lista nao esquecam de mudar na funcao de inicializacao
 	//ORDEM DE DIRECOES UP DOWN RIGHT LEFT
 	int dir;
-	float posx;
-	float posy;
+	Rectangle rect;
 	Texture2D sprite;
+	Vector2 vect;
 	struct balls* next;
 	struct balls* prev;
 
@@ -37,25 +37,68 @@ void getdirectionofball(int* val){
 
 }
 
-void moveball(float *posx, float* posy){
+void moveball(balls** head){
+
+	if((*head)->dir == 1){
+		(*head)->rect.y++;
+		(*head)->vect.y++;
+
+	}
+	if((*head)->dir == 2){
+		(*head)->rect.y--;
+		(*head)->vect.y--;
+	}
+	if((*head)->dir == 3){
+		(*head)->rect.x++;
+		(*head)->vect.x++;
+	}
+	if((*head)->dir == 4){
+		(*head)->rect.x--;
+		(*head)->vect.x--;
+	}
 
 }
 
 
-void createnextball(balls** head,balls** tail, int type,Texture2D sprite, float posx, float posy){
+void createnextball(balls** head,balls** tail, int type,Texture2D sprite){
 
 	if(*head == NULL) {
+
 
 		*head = (balls*)malloc(sizeof(balls));
 		if (*head == NULL) return;
 		(*head)->next = NULL;
 		(*head)->type = type;
 
+		int posx;
+		int posy;
+
 		getdirectionofball(&(*head)->dir);
 		
-		(*head)->posx = posx;
-		(*head)->posy = posy;
+		if((*head)->dir == 1){
+			posx = GetScreenWidth()/2;
+			posy = 0;
+		}
+		if((*head)->dir == 2){
+			posx = GetScreenWidth()/2;
+			posy = GetScreenHeight;
+		}
+		if((*head)->dir == 3){
+			posx = GetScreenWidth();
+			posy = GetScreenHeight()/2;
+		}
+		if((*head)->dir == 4){
+			posx = 0;
+			posy = GetScreenHeight()/2;
+		}
+		
+		(*head)->rect.x = posx;
+		(*head)->rect.y = posy;
+		(*head)->rect.height = sprite.height;
+		(*head)->rect.width = sprite.width;
 		(*head)->sprite = sprite;
+		(*head)->vect.x = posx;
+		(*head)->vect.y = posy;
 		*tail = *head;
 		(*head)->prev = NULL;
 		return;
@@ -68,9 +111,33 @@ void createnextball(balls** head,balls** tail, int type,Texture2D sprite, float 
 	
 	getdirectionofball(&(n->next->dir));
 	
-	n->next->posx = posx;
-	n->next->posy = posy;
+	int posx;
+	int posy;
+
+	if(n->next->dir == 1){
+			posx = GetScreenWidth()/2;
+			posy = 0;
+		}
+		if(n->next->dir == 2){
+			posx = GetScreenWidth()/2;
+			posy = GetScreenHeight;
+		}
+		if(n->next->dir == 3){
+			posx = GetScreenWidth();
+			posy = GetScreenHeight()/2;
+		}
+		if(n->next->dir == 4){
+			posx = 0;
+			posy = GetScreenHeight()/2;
+		}
+
+	n->next->rect.x = posx;
+	n->next->rect.y = posy;
+	n->next->rect.height = sprite.height;
+	n->next->rect.width = sprite.width;
 	n->next->sprite = sprite;
+	n->next->vect.x = posx;
+	n->next->vect.y = posy;
 	n->next->next = NULL;
 	n->next->prev = n;
 	*tail = n->next;
@@ -128,12 +195,13 @@ int main ()
 	balls* head = NULL;
 	balls* tail = NULL;
 	balls* aux = NULL;
+	balls* n = NULL;
 
 	
 
 	for(int i = 0; i < praiera.qntbeats; i++){
-		//random da funcao n ta funcionando
-		createnextball(&head,&tail, 0,LoadTexture("balltest.png"),0,0);
+
+		createnextball(&head,&tail, 0,LoadTexture("balltest.png"));
 
 	}
 
@@ -146,6 +214,13 @@ int main ()
 	float pposx = screenwidth/2;
 	float pposy = screenheight/2;
 
+	Rectangle playerrect;
+
+	playerrect.height = wabbit.height;
+	playerrect.width = wabbit.width;
+	playerrect.x = pposx;
+	playerrect.y = pposy;
+
 	//variaveis de onde o jogador vai apontar
 	int up = 0;
 	int down = 0;
@@ -154,6 +229,7 @@ int main ()
 
 
 	aux = head;
+	n = head;
 	// game loop
 	while (!WindowShouldClose())		
 	{
@@ -189,6 +265,13 @@ int main ()
 		if(IsKeyPressed(KEY_SPACE) && aux->next != NULL ) aux = aux->next;
 
 
+		if(IsKeyPressed(KEY_BACKSPACE) && aux->prev != NULL) aux = aux->prev;
+
+
+		if(n->prev != NULL && CheckCollisionRecs(n->rect,n->prev->rect)){
+			moveball(n);
+		}
+
 		BeginDrawing();
 
 		UpdateMusicStream(praiera.musica);
@@ -210,6 +293,8 @@ int main ()
 		if(aux->dir == 3)DrawText("direita",400, 400, 20, WHITE);
 		if(aux->dir == 4)DrawText("esquerda",400, 400, 20, WHITE);
 
+		
+		DrawTextureRec(n->sprite, n->rect, n->vect, WHITE);
 
 		// posicao onde o jogador vai pegar as notas
 		if(up)DrawRectangle(pposx, pposy - 20, 10, 2, RED);
